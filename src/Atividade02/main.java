d519eeead961387a7afe164b6c1ec37092a65315
gustavouliano <gustavouliano11@gmail.com>
2024-03-11 22:33:00 -0300

    Ajustes questão 2

--- src/Atividade02/main.java
+++ src/Atividade02/main.java
@@ -1,13 +1,20 @@
 package Atividade02;
 
+import java.util.Scanner;
+
 public class main {
 
 	public static void main(String[] args) {
 		
-		int qtd_thread = 3;
+		Scanner input = new Scanner(System.in);
 		
+		System.out.print("Informe um valor maior que 5000: ");
+		int valor = input.nextInt();
+		input.close();
+		
+		int qtd_thread = 10;
 		for(int i = 0; i < qtd_thread; i++) {
-			new Contador(50);
+			new Contador(valor);
 		}
 		
 	}
